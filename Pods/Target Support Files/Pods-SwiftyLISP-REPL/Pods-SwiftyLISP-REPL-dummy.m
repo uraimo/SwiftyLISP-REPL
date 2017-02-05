271b1bdd637234ed138aee5d3b28ad0d8c445e11
Umberto Raimondi <me@uraimo.com>
2017-02-05 22:35:28 +0100

    Added .gitignore

--- Pods/Target Support Files/Pods-SwiftyLISP-REPL/Pods-SwiftyLISP-REPL-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftyLISP_REPL : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftyLISP_REPL
-@end
--- Pods/Target Support Files/SwiftyLisp/SwiftyLisp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftyLisp : NSObject
-@end
-@implementation PodsDummy_SwiftyLisp
-@end
