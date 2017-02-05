4628f8e986b5489a4470c27a08c6da249014fd84
Umberto Raimondi <me@uraimo.com>
2017-02-05 22:09:55 +0100

    Initial Commit

---
+++ SwiftyLISP-REPL/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftyLISP-REPL
+//
+//  Created by Umberto Raimondi on 05/02/2017.
+//  Copyright © 2017 Umberto Raimondi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
